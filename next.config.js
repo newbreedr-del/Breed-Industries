cee570f5ad545d3889b160043f99e6ea12b747ac
Your Name <your-email@example.com>
2026-01-11 13:53:41 +0200

    Fix project structure for Hostinger compatibility

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   swcMinify: false,
   reactStrictMode: true,
+  output: 'standalone',
+  poweredByHeader: false,
+  env: {
+    APP_ENV: process.env.NODE_ENV,
+  },
 }
 
 module.exports = nextConfig
